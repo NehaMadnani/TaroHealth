6292c9bb9428981f33ab58249d43e1f59f17db54
Neha Suresh <mailnehasuresh@gmail.com>
2024-11-17 18:05:55 -0800

    Initial Commit

---
+++ Taro Health/Taro_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taro_HealthApp.swift
+//  Taro Health
+//
+//  Created by Neha Suresh on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Taro_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Taro HealthTests/Taro_HealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  Taro_HealthTests.swift
+//  Taro HealthTests
+//
+//  Created by Neha Suresh on 11/17/24.
+//
+
+import Testing
+@testable import Taro_Health
+
+struct Taro_HealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
